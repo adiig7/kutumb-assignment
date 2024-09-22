fceb5da464867c117c9fa73916637001c27d8058
adiig7 <adityagupta765469@gmail.com>
2024-09-22 21:58:40 +0530

    [UPDATE] date type in quote meta

--- src/components/molecules/QuoteMeta.jsx
+++ src/components/molecules/QuoteMeta.jsx
@@ -4,7 +4,7 @@ const QuoteMeta = ({ quoteAuthor, quoteCreateDate }) => {
   return (
     <div className="flex max-w-full h-8 justify-between items-center text-xs rounded-b-md p-2 bg-white">
       <span className="truncate">{quoteAuthor}</span>
-      <span>{new Date(quoteCreateDate).toLocaleDateString()}</span>
+      <span>{new Date(quoteCreateDate).toDateString()}</span>
     </div>
   );
 };
